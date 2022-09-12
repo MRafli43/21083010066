44541fce18acb6235f2a730a45eeb565b95d9ce6
MRafli43 <21083010066@student.upnjatim.ac.id>
2022-09-12 21:47:12 +0700

    Tugas-1.py

---
+++ Tugas-1.py
@@ -0,0 +1,9 @@
+print("-" * 20)
+print("Tugas 1 SISOP Kelas-A")
+print("-" * 20)
+print("Nama : Muhammad Rafli")
+print("NPM : 21083010066")
+print("-" * 20)
+print("Halo Dunia!")
+print("Ini adalah script bash pertamaku di Linux")
+
